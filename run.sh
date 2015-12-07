5c4146c3137e761a68c14dbd615df5b22777a4a9
Alexander Nikolaev <variomap@gmail.com>
2015-12-07 16:16:51 +0300

    Restructured project as a debian package source

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tmpfile=`mktemp deleteme.XXXXXXXXXX`
-
-/usr/bin/stdbuf -oL /home/fairwaves/meas_json > $tmpfile &
-exec /usr/bin/websocketd --staticdir=. --port=8080 tail -F $tmpfile
-#rm $tmpfile
